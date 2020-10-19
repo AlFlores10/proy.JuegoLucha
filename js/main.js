2df69146eb1625ffcd56695d89e6cfbcd09a5213
AlFlores10 <alberto.flores.1990@gmail.com>
2020-10-19 07:59:32 +0200

    juego terminado

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //Declaracion de variables del juego
 
 let arrayPersonajes = [];
-
+let turno = 1;
 
 //Declaro las funciones
 
@@ -13,12 +13,48 @@ const botonOcultar = () => {
     selectorPlayer.style.display = "none";
 }
 
+let golpear = () => {
+    let hit = (arrayPersonajes[0].ataque - arrayPersonajes[1].defensa);
+    arrayPersonajes[1].vida -= hit; 
 
+    let hit2 = (arrayPersonajes[1].ataque - arrayPersonajes[0].defensa);
+    arrayPersonajes[0].vida -= hit2; 
 
-// const botonClick = (arg1) => {
+    console.log(`${arrayPersonajes[0].nombre} hace ${hit} de daño`);
+    console.log(`${arrayPersonajes[1].nombre} hace ${hit2} de daño`);
+
+    combateLog.innerHTML = (`TURNO: ${turno} </br></br>${arrayPersonajes[0].nombre} hace ${hit} de daño </br> Vida Restante ${arrayPersonajes[1].nombre} = ${arrayPersonajes[1].vida} </br></br>     
+    ${arrayPersonajes[1].nombre} hace ${hit2} de daño </br> Vida Restante ${arrayPersonajes[0].nombre} = ${arrayPersonajes[0].vida}`);
     
-//     console.log("Me has clickado y me has pasado el numero :", arg1, " como argumento.");
-// }
+    console.log(`${arrayPersonajes[0].nombre} Vida: ${arrayPersonajes[0].vida}`);
+    console.log(`${arrayPersonajes[1].nombre} Vida: ${arrayPersonajes[1].vida}`);
+
+
+        if((arrayPersonajes[0].vida > 0) && (arrayPersonajes[1].vida > 0)){
+        turno++;
+        
+    
+        }else if(arrayPersonajes[1].vida <= 0) {
+
+        selector.style.display = "none";
+        selectorPlayer.style.display = "none";
+        combateLog.style.display = "none";
+        finalPartida.style.display = "block";
+        btnGolpear.style.display = "none";
+        alert("JUEGO TERMINADO. J1 WIN");
+        }
+        else{
+            selector.style.display = "none";
+            selectorPlayer.style.display = "none";
+            combateLog.style.display = "none";
+            finalPartida.style.display = "block";
+            btnGolpear.style.display = "none"; 
+            alert("JUEGO TERMINADO. J2 WIN");
+        }
+    return;
+}
+
+
 
 const selectorPersonaje = (personaje) => {
     
@@ -33,6 +69,8 @@ const selectorPersonaje = (personaje) => {
             console.log("Ya has escogido los 2 personajes",arrayPersonajes);
 
             selector.style.display = "none";
+            combateLog.style.display = "block";
+            btnGolpear.style.display = "block";
         
             return;
         }
@@ -48,5 +86,6 @@ const selectorPersonaje = (personaje) => {
 
 //Ejecución del código
 
-// divTexto.innerHTML = "Imagen Azul";
-// divTexto.style.color = "blue";
+
+
+
